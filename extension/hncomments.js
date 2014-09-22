22b2fba7044254926b054d0e413aa8b55bdd1f64
Michael Josephson <mike@josephson.org>
2014-09-22 22:36:10 +0100

    Initial commit

---
+++ extension/hncomments.js
@@ -0,0 +1,26 @@
+function searchHN(url) {
+    $.getJSON("https://hn.algolia.com/api/v1/search?tags=story&query=" + encodeURI(url), function(data) {
+        processResults(data, url)
+    }); 
+}
+
+function processResults(data, url) {
+    var match = _.find(data.hits, function(hit) { return hit.url == url; });
+    if (match) {
+        var commentURL = 'https://news.ycombinator.com/item?id=' + match.objectID;
+        chrome.tabs.update({ url: commentURL });
+    } else {
+        window.alert('No discussion of ' + url + ' found');
+    }
+}
+
+chrome.commands.onCommand.addListener(function(command) {
+    if (command === 'find-hn-comments') {
+        chrome.tabs.getSelected(null, function (tab) {
+            if (tab.url.substring(0,9) == 'chrome://') {
+                return;
+            }
+            searchHN(tab.url);
+        });
+    };
+});
